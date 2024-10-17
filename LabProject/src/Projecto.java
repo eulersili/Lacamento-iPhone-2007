2652b658a16df5a71e5720c324b014643e2ff7d3
eulersili <silieuler@gmail.com>
2024-10-17 15:11:52 +0100

    first commit

---
+++ LabProject/src/Projecto.java
@@ -0,0 +1,5 @@
+public class Projecto {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ LabProject/src/iPod.java
@@ -0,0 +1,14 @@
+import LabProject001.iPhone;
+
+public class iPod extends iPhone {
+
+    void tocar() {
+        System.out.println("Faixa tocando.");
+    }
+    void pausar() {
+        System.out.println("Som pausado.");
+    }
+    void selecionarMusica (String musica) {
+        System.out.println("Música selecionada.");
+    }
+}
